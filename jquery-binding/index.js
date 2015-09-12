32b309aea89e0072e0da23ecd6271e4399eaaa24
vhnam <vhnam2504@gmail.com>
2015-09-12 15:56:17 +0700

    add feature remove artist and initial for jquery-binding project.

--- jquery-binding/index.js
+++ jquery-binding/index.js
@@ -12,33 +12,72 @@
             }
         }
 
-        $('#btn-insert').click(function() {
-            // Get data
-            var name = $('#artist-name').val(),
-                instrument = $('#artist-instrument').val();
+        function removeArtist() {
+            // Remove selected row
+            var row  = $(this).parents()[1];
+            $(row).remove();
 
+            // Set index
+            setIndex();
+        }
+
+        function insertRow(artist) {
             // Create new elements
             var row = document.createElement('tr'),
                 indexColumn = document.createElement('td'),
                 artistColumn = document.createElement('td'),
-                instrumentColumn = document.createElement('td');
+                instrumentColumn = document.createElement('td'),
+                buttonColumn = document.createElement('td'),
+                removeButton = document.createElement('button'),
+                removeIcon = document.createElement('span');
 
             // Set data
-            artistColumn.innerHTML = name;
-            instrumentColumn.innerHTML = instrument;
+            artistColumn.innerHTML = artist.name;
+            instrumentColumn.innerHTML = artist.instrument;
+
+            // Create remove button
+            $(removeIcon).addClass('glyphicon glyphicon-remove');
+            $(removeButton).addClass('btn btn-danger btn-remove');
+            $(removeButton).attr('type', 'button');
+            removeButton.addEventListener('click', removeArtist);
+            removeButton.appendChild(removeIcon);
+            buttonColumn.appendChild(removeButton);
 
             // Append children
             row.appendChild(indexColumn);
             row.appendChild(artistColumn);
             row.appendChild(instrumentColumn);
+            row.appendChild(buttonColumn);
             $('#list-artist').append(row);
 
             // Set index
             setIndex();
+        }
+
+        $('#btn-insert').click(function() {
+            // Create an artist
+            var artist = {
+                'name' : $('#artist-name').val(),
+                'instrument' : $('#artist-instrument').val()
+            };
+
+            // Insert into list
+            insertRow(artist);
 
             // Clear data in form
             $('#artist-name').val('');
             $('#artist-instrument').val('');
+        });
+
+        // Initial
+        var artists = [
+            { 'name': 'Louis Armstrong', 'instrument': 'Trumpet' },
+            { 'name': 'Kenny G', 'instrument': 'Saxophone' },
+            { 'name': 'Lucia Micarelli', 'instrument': 'Violin' }
+        ];
+
+        artists.forEach(function(artist) {
+            insertRow(artist);
         });
     });
 })();
