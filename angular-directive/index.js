b4f46ce5d5a61565ba6a00d44032b7718a5bd1ea
vhnam <vhnam2504@gmail.com>
2015-09-13 17:30:19 +0700

    add feature post status for angular-directive project.

--- angular-directive/index.js
+++ angular-directive/index.js
@@ -5,14 +5,42 @@
 
         .directive('status', function() {
             return {
-                restrict: 'E',
-                templateURL: 'status.html'
+                restrict: 'AE',
+                scope: {
+                    time: '@',
+                    paragraph: '@',
+                    image: '@'
+                },
+                templateUrl: 'status.html'
             }
         })
 
+        .filter('reverse', function() {
+            return function(items) {
+                return items.slice().reverse();
+            };
+        })
+
         .controller('TimelineController', ['$scope', function($scope) {
+
             // Autosize - https://github.com/jackmoore/autosize/
             autosize(document.querySelector('#status-content'));
+
+            // Initial
+            $scope.timeline = [];
+
+            // Post status
+            $scope.postStatus = function() {
+                var status = {
+                    time: (new Date()).toLocaleString(),
+                    paragraph: $scope.paragraph,
+                    image: $scope.image
+                }
+                $scope.timeline.push(status);
+
+                $scope.paragraph = null;
+                $scope.image = null;
+            }
         }])
     ;
 })();
