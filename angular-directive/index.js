ee44cbef9efaae86be98feb4c94193bb7018a633
vhnam <vhnam2504@gmail.com>
2015-09-14 15:51:39 +0700

    angular-directive project: fix bind html error.

--- angular-directive/index.js
+++ angular-directive/index.js
@@ -1,8 +1,58 @@
 (function() {
     'use strict';
 
-    angular.module('data-directive', [])
+    angular.module('data-directive', ['monospaced.elastic'])
 
+        // Reverse timeline when insert a status
+        .filter('reverse', function() {
+            return function(items) {
+                return items.slice().reverse();
+            };
+        })
+
+        // Process paragraph
+        .filter('paragraph', function() {
+            return function(content) {
+                var paragraph = content.split('\n'),
+                    newContent  = '';
+
+                paragraph.forEach(function(text) {
+                    newContent += '<p>' + text + '</p>';
+                })
+
+                return newContent;
+            }
+        })
+
+        /**
+         * Binding unsafe HTML
+         *
+         * @author wickY26
+         * @link http://stackoverflow.com/users/3324474/wicky26
+         */
+        .directive('bindUnsafeHtml', ['$compile', function ($compile) {
+            return function(scope, element, attrs) {
+                scope.$watch(
+                    function(scope) {
+                        // watch the 'bindUnsafeHtml' expression for changes
+                        return scope.$eval(attrs.bindUnsafeHtml);
+                    },
+                    function(value) {
+                        // when the 'bindUnsafeHtml' expression changes
+                        // assign it into the current DOM
+                        element.html(value);
+
+                        // compile the new DOM and link it to the current
+                        // scope.
+                        // NOTE: we only compile .childNodes so that
+                        // we don't get into infinite loop compiling ourselves
+                        $compile(element.contents())(scope);
+                    }
+                );
+            };
+        }])
+
+        // Status template
         .directive('status', function() {
             return {
                 restrict: 'AE',
@@ -15,16 +65,8 @@
             }
         })
 
-        .filter('reverse', function() {
-            return function(items) {
-                return items.slice().reverse();
-            };
-        })
-
+        // Timeline Controller
         .controller('TimelineController', ['$scope', function($scope) {
-
-            // Autosize - https://github.com/jackmoore/autosize/
-            autosize(document.querySelector('#status-content'));
 
             // Initial
             $scope.timeline = [];
