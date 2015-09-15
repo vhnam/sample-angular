18e6bd9b460c11b45b74b06f62292f6f23c4bf39
vhnam <vhnam2504@gmail.com>
2015-09-15 15:30:13 +0700

    fix config routing in angular-spa project.

--- angular-spa/index.js
+++ angular-spa/index.js
@@ -1,15 +1,31 @@
 (function() {
     'use strict';
 
-    angular.module('data-spa', [])
-        .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.
-            when('/', {
-                templateUrl: 'introduction.html'
-            }).
-            otherwise({
-                redirectTo: '/'
+    angular.module('data-spa', ['ngRoute'])
+
+        // Caching template
+        .run(['$http', '$templateCache', function($http, $templateCache) {
+            $http.get('introduction.html', {
+                cache: $templateCache
             });
+            $http.get('about.html', {
+                cache: $templateCache
+            });
+        }])
+
+        // Config routing
+        .config(['$routeProvider', function($routeProvider) {
+            $routeProvider.
+                when('/', {
+                    templateUrl: 'introduction.html'
+                }).
+                when('/about', {
+                    templateUrl: 'about.html'
+                }).
+                otherwise({
+                    redirectTo: '/'
+                })
+            ;
         }])
     ;
 })();
